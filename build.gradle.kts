1ace757febd4d6f3232ce5f4440b220ff7865429
Jonathan Ruiz <jonnyjonason@gmail.com>
2022-05-14 19:19:00 +0200

    add missing jdbc driver / database dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-stream")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
